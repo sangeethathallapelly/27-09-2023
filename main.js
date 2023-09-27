7a77a375d79aa08a225a378d0d99f019ee76c49f
sangeethathallapelly <146064909+sangeethathallapelly@users.noreply.github.com>
2023-09-27 17:11:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+function add(){
+    if(true){
+        var a="100";
+       
+        document.getElementById("varOutput").textContent=a;
+       
+    }
+    if(true){
+        let b="300";
+      
+         document.getElementById("letOutput").textContent=b;
+       
+    }
+    if(true){
+         const c="500";
+       
+       
+        document.getElementById("constOutput").textContent=c;
+       
+    }
+}
+function change(){
+    if(true){
+       var a="200";
+         document.getElementById("varOutput1").textContent=a;
+        
+    }
+    if(true){
+        let  b="400";
+         document.getElementById("letOutput1").textContent=b;
+        
+    }
+    if(true){
+         const c="600";
+       
+       
+        document.getElementById("constOutpu1t").textContent=c;
+        
+    }
+}
+
+ 
